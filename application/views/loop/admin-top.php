1bdcd2011e3250af120e6b68303b15bb0b027546
Jorge Garibaldo GD <biohizard@gmail.com>
2020-06-19 03:29:21 -0500

    caja
    caja update

--- application/views/loop/admin-top.php
+++ application/views/loop/admin-top.php
@@ -69,6 +69,13 @@
                 </a>
               </li>
               <li class="nav-item">
+                <a class="nav-link" href="<?php echo INDEX_PAGE; ?>caja/?since=origin">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
+                  Caja
+                </a>
+              </li>
+                            
+              <li class="nav-item">
                 <a class="nav-link" href="<?php echo INDEX_PAGE; ?>usuarios/?since=origin">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                   Usuarios
@@ -86,12 +93,7 @@
                   Provedores
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo INDEX_PAGE; ?>caja/?since=origin">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
-                  Caja
-                </a>
-              </li>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
