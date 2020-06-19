38cac6e7fdccb92be86925c3d9de9403bb5c332d
Jorge Garibaldo GD <biohizard@gmail.com>
2020-06-19 18:04:10 -0500

    Update 0-top.php
    Update
    *hidde a hidden

--- application/views/caja/0-top.php
+++ application/views/caja/0-top.php
@@ -30,8 +30,8 @@
         <button type="button" class="btn btn-primary" id="user-resume" data-toggle="modal" data-target=".bd-example-modal-lg" disabled>Resumen</button>
 
         
-        <input type="hidde" id="saldoinicial" placeholder="Saldo Inicial"            value="" disabled>
-        <input type="hidde" id="saldofinal"   placeholder="Saldo Final"              value="" disabled>
-        <input type="hidde" id="ultimafecha"  placeholder="Ultima Fecha de Registro" value="" disabled>
+        <input type="hidden" id="saldoinicial" placeholder="Saldo Inicial"            value="" disabled>
+        <input type="hidden" id="saldofinal"   placeholder="Saldo Final"              value="" disabled>
+        <input type="hidden" id="ultimafecha"  placeholder="Ultima Fecha de Registro" value="" disabled>
 
       </div>      
